43d13fcd5896e6844d56648d8a345e9efb7719f3
nyiliang <nyiliang@dsta.gov.sg>
2026-01-07 16:10:19 +0800

    Update backend state to use tf remote

--- main.tf
+++ main.tf
@@ -6,12 +6,19 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket         = "tfstate-975050193152"
-    dynamodb_table = "tf-lock"
-    key            = "main/terraform.tfstate"
-    region         = "ap-southeast-1"
+  backend "remote" {
+    organization = "rnyl_personal"
+    workspaces {
+      name = "terraform-github-actions"
+    }
   }
+
+  # backend "s3" {
+  #   bucket         = "tfstate-975050193152"
+  #   dynamodb_table = "tf-lock"
+  #   key            = "main/terraform.tfstate"
+  #   region         = "ap-southeast-1"
+  # }
 
   # backend "local" {}
 }
